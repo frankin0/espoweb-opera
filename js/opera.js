0d748b8c437c7d850d4e1781848b910fa7711ead
Francesco Esposito <francescoe15@gmail.com>
2022-02-13 13:34:39 +0100

    ipdate operajs

--- js/opera.js
+++ js/opera.js
@@ -1,110 +1,148 @@
-'use strict';  
 
 /**
  * OperaJS V Build 1
  */
 
- ;(function($) {
-    /*
-     *  Form
-     */
-    var Mask = {
-		init : function(options){
-			var defaults = {};
-			options = $.extend(defaults, options);
+/*
+    *  Form
+    */
+var Mask = {
+    
+    init : function(options){
+        var defaults = {
+            dashItms: ['/', '-', ' ', '[', ']', '!', '@', '#', '$', '%', '&', '*', '(', ')', '=', '+', '/\/', '^', '?', '{', '}', ':', ';', ',', '_', '>', '<', '\'', '.']
+        };
 
-			return this.each(function(i){
-				//variables
-				var i;
-                
-                
-                for (let element of this.element) {
-                    //check mask element
-                    if(element.type == "text"){
-                        if(element.dataset.mask){
-                            element.setAttribute('maxlength', element.dataset.mask.length);
-                            element.setAttribute('placeholder', element.dataset.mask);
-                            element.addEventListener('keydown', this.onKeyDownMask);
-                            element.addEventListener('input', this.onInputMask, false);
-                            element.ptrns = Form.getPattern(element.dataset.mask);
-                            let mask = element.dataset.mask;
-        
-                            console.log(Form.getPatternChars(mask), element.ptrns, mask);
-                        }
-                    }
-                    
-                }
-            });
-		},
-        getPatternChars(mask){
-            let arr = [];
-            Form.getPattern(mask).forEach(element => {
-                arr.push(mask.substr(element, 1));
-            });
-            
-            return arr;
-    
-        },
-        getPattern(pattern) {
-            let dashIdxs = [];
-        
-            pattern.split("").forEach((char, idx) => {
-                if (Form.dashItms.indexOf(char) == -1) {
-                    return;
-                }
-    
-        
-                dashIdxs.push(idx);
-            });
-           
-            return dashIdxs;
-        },
-        md(str){
-            return m.trust(marked(str));
-        },
-        onKeyDownMask(e) {
-            //console.log(e.target.ptrns);
-            if (e.key === "Backspace" && e.target.ptrns.includes(e.target.value.length - 1)) {
-                e.target.value = e.target.value.slice(0, -1);
-                Form.x --;
-            }
-        },
-        onInputMask(e, me = Form){
-            String.prototype.replaceAt = function(index, replacement) {
-                return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-            }
-            
-            let value = e.target.value;
-    
-            let mask = e.target.dataset.mask.replace(/[^a-zA-Z0-9 ]/g, "");
-            if(mask.match(/^[0-9]+$/) != null){
-                e.target.value = e.target.value.replace(/[^0-9./]/g, '');
-            }
-        
-            var xx = me.getPatternChars(e.target.dataset.mask);
-            let stringL = value.length;
-            
-            if (e.target.ptrns.includes(stringL)) {
-                console.log(Form.x);
-                e.target.value += xx[Form.x];
-                Form.x++;
+        console.log(options);
+        /*var tempData = {};
+        for ( var index=0; index<options.length; index++ ) {
+            if ( options[index]) {
+
+                tempData.push(options );
             }
         }
-	};
-	
-    $.fn.mask = function (options){
-        if(Mask[options]){
-            return Mask[options].apply(this, Array.prototype.slice.call(arguments, 1));
-        }else if(typeof options === 'object' || !options){
-            return Mask.init.apply(this, arguments);
-        }else{
-            console.error('This method is not recognized [' + options + ']');
+
+
+        options = options.push(defaults);//$.extend(defaults, options); */
+return;
+        return this.each(function(element){
+            //variables
+            var i;
+            
+            if(element.type == "text"){
+                if(element.dataset.mask){
+                    element.setAttribute('maxlength', element.dataset.mask.length);
+                    element.setAttribute('placeholder', element.dataset.mask);
+                    element.addEventListener('keydown', this.onKeyDownMask);
+                    element.addEventListener('input', this.onInputMask, false);
+                    element.ptrns = Mask.getPattern(element.dataset.mask);
+                    let mask = element.dataset.mask;
+                    
+                    console.log(element);
+                    //console.log(Mask.getPatternChars(mask), element.ptrns, mask);
+                }
+            }
+            
+        });
+    },
+    getPatternChars(mask){
+        let arr = [];
+        Mask.getPattern(mask).forEach(element => {
+            arr.push(mask.substr(element, 1));
+        });
+        
+        return arr;
+
+    },
+    getPattern(pattern) {
+        let dashIdxs = [];
+    
+        pattern.split("").forEach((char, idx) => {
+            if (Mask.dashItms.indexOf(char) == -1) {
+                return;
+            }
+
+    
+            dashIdxs.push(idx);
+        });
+        
+        return dashIdxs;
+    },
+    md(str){
+        return m.trust(marked(str));
+    },
+    onKeyDownMask(e) {
+        //console.log(e.target.ptrns);
+        if (e.key === "Backspace" && e.target.ptrns.includes(e.target.value.length - 1)) {
+            e.target.value = e.target.value.slice(0, -1);
+            Mask.x --;
+        }
+    },
+    onInputMask(e, me = Mask){
+        String.prototype.replaceAt = function(index, replacement) {
+            return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+        }
+        
+        let value = e.target.value;
+
+        let mask = e.target.dataset.mask.replace(/[^a-zA-Z0-9 ]/g, "");
+        if(mask.match(/^[0-9]+$/) != null){
+            e.target.value = e.target.value.replace(/[^0-9./]/g, '');
+        }
+    
+        var xx = me.getPatternChars(e.target.dataset.mask);
+        let stringL = value.length;
+        
+        if (e.target.ptrns.includes(stringL)) {
+            console.log(Mask.x);
+            e.target.value += xx[Mask.x];
+            Mask.x++;
         }
     }
-    $(document).ready(function(){
-		$('input[data-mask]').mask();
-    });
-}());
+};
+
+function $(selector){
+    const self = {
+        element: document.querySelectorAll(selector), 
+        html: ()=> self.element,
+        each: (callback) => {
+            self.element.forEach(i => {
+                callback(i, self.element);
+            });
+        },
+        on: (event, callback) => {
+            document.addEventListener(event, callback)
+        },
+        hide: () => {
+            self.element.style.display = "none"
+        },
+        attr: (name, value) => {
+            if(value == null){
+                self.element.getAttribute(name);
+            }else{
+                self.element.getAttribute(name, value);
+            }
+        },
+        mask: (options) =>{
+            if(Mask[options]){
+                Mask[options].apply(self, Array.prototype.slice.call(arguments, 1));
+            }else if(typeof options === 'object' || !options){
+                Mask.init.apply(options);
+            }else{
+                console.error('This method is not recognized [' + options + ']');
+            }
+        }
+    } 
+
+    return self;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+    $('input[data-mask]').mask({
+        test: 'ok'
+    })
+});
 
 
 /***
